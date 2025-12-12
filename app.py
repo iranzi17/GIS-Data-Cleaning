6d77fedc6eef0805734267e0f75827746cdfd906
iranzi17 <50293498+iranzi17@users.noreply.github.com>
2025-12-12 20:20:36 +0200

    jkjjjjk

--- app.py
+++ app.py
@@ -1253,6 +1253,14 @@ FILE_MATCH_OVERRIDES = {
         "Substation ID",
         "Feeder Type",
     ],
+    normalize_for_compare("INDOR SWITCHGEAR TABLE"): [
+        "Feeder_ID",
+        "FeederID",
+        "Feeder_Name",
+        "FeederName",
+        "SwitchGearID",
+        "Switchgear ID",
+    ],
     normalize_for_compare("SWITCHGEAR"): [
         "FeederID",
         "Feeder ID",
